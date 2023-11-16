505bb66dbfd24d28502bb78d62282bb637a1ab33
youri <ykarpeev@permanentmarking.com>
2023-11-16 16:23:34 -0500

    repo

---
+++ y.hex.convert/HexConversion.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="HexConversion.cs" company="Youri">
+// Copyright (c) Youri. All rights reserved.
+// </copyright>
+
+namespace y.hex.convert
+{
+    using System;
+    using System.Buffers.Binary;
+
+    /// <summary>
+    /// Hex to integer conversion supporting signed 2's complement.
+    /// </summary>
+    public static class HexConversion
+    {
+        /// <summary>
+        /// Convert from hex string to an integer.
+        /// </summary>
+        /// <param name="hex">Hex value.</param>
+        /// <returns>Integer value.</returns>
+        public static short FromHex(this string hex)
+        {
+            var ba = Enumerable.Range(0, hex.Length / 2).
+                Select(x => Convert.ToByte(hex.Substring(x * 2, 2), 16));
+
+            var span = new ReadOnlySpan<byte>(ba.Reverse().ToArray());
+
+            return BinaryPrimitives.ReadInt16LittleEndian(span);
+        }
+    }
+}
---
+++ y.hex.convertTests/HexConversionTests.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="HexConversionTests.cs" company="Youri">
+// Copyright (c) Youri. All rights reserved.
+// </copyright>
+
+namespace y.hex.convert.Tests
+{
+    using NUnit.Framework;
+    using y.hex.convert;
+
+    [TestFixture()]
+    public class HexConversionTests
+    {
+        [TestCase("FFFFD8F0", -10000)]
+        [TestCase("FFEE", -18)]
+        [TestCase("00F1", 241)]
+        public void FromHexTest(string hexString, int expected)
+        {
+            Assert.That(hexString.FromHex, Is.EqualTo(expected));
+        }
+    }
+}
