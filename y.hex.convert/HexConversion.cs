162ea7d9dd095da8a8a0119c8011c7438bef0b21
youri <ykarpeev@permanentmarking.com>
2023-11-17 12:12:21 -0500

    handle a case where we have an int

--- y.hex.convert/HexConversion.cs
+++ y.hex.convert/HexConversion.cs
@@ -6,6 +6,8 @@ namespace y.hex.convert
 {
     using System;
     using System.Buffers.Binary;
+    using System.Diagnostics;
+    using System.Text;
 
     /// <summary>
     /// Hex to integer conversion supporting signed 2's complement.
@@ -17,14 +19,21 @@ namespace y.hex.convert
         /// </summary>
         /// <param name="hex">Hex value.</param>
         /// <returns>Integer value.</returns>
-        public static short FromHex(this string hex)
+        public static int FromHex(this string hex)
         {
             var ba = Enumerable.Range(0, hex.Length / 2).
                 Select(x => Convert.ToByte(hex.Substring(x * 2, 2), 16));
 
             var span = new ReadOnlySpan<byte>(ba.Reverse().ToArray());
 
-            return BinaryPrimitives.ReadInt16LittleEndian(span);
+            if (hex.Length == 4)
+            {
+                return BinaryPrimitives.ReadInt16LittleEndian(span);
+            }
+            else
+            {
+                return BinaryPrimitives.ReadInt32LittleEndian(span);
+            }
         }
     }
 }
--- y.hex.convertTests/HexConversionTests.cs
+++ y.hex.convertTests/HexConversionTests.cs
@@ -13,6 +13,7 @@ namespace y.hex.convert.Tests
         [TestCase("FFFFD8F0", -10000)]
         [TestCase("FFEE", -18)]
         [TestCase("00F1", 241)]
+        [TestCase("00008C09", 35849)]
         public void FromHexTest(string hexString, int expected)
         {
             Assert.That(hexString.FromHex, Is.EqualTo(expected));
